0a2c3ca13e64bf3c1a58d9e7870d053a719eedb0
Bibas-bk <rambk12353@gmail.com>
2026-01-07 21:28:13 +0545

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main() {
+    Stack s;
+    initStack(&s);
+
+    push(&s, 10);
+    push(&s, 20);
+    push(&s, 30);
+
+    display(&s);
+
+    printf("Top = %d\n", peek(&s));
+
+    printf("Popped = %d\n", pop(&s));
+    display(&s);
+
+    return 0;
+}
