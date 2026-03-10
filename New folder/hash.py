9e994141b0b9ec230df60924ce22b2c026edc88c
Bibas-bk <rambk12353@gmail.com>
2026-03-10 23:27:56 +0545

    try to build hash table

--- New folder/hash.py
+++ New folder/hash.py
@@ -4,24 +4,23 @@ class HashTable:
     def hash(self,string):
         total=0
         for char in string:
-            total+=ord(char)# ord() function returns the unicode code point for a given character
+            total+=ord(char)
         return total        
     def add(self,key,value):
-        hash_value=self.hash(key)# compute the hash value for the given key
-        if hash_value not in self.collection:#check if the hash value alreadyexists int the collection
-            self.collection[hash_value]={}#if the hash value does not exist,create a new dictionary at the hash value
+        hash_value=self.hash(key)
+        if hash_value not in self.collection:
+            self.collection[hash_value]={}
 
-        self.collection[hash_value][key]=value #add the key value pair to the dictionary at the hash value and it also handles collision s   
+        self.collection[hash_value][key]=value  
     def remove(self,key):
         hash_value=self.hash(key)
-        if hash_value in self.collection and key in self.collection[hash_value]:#check if the hash value exists in the collections and key exists in the dictionary at the hash value.
-            del self.collection[hash_value][key]#renove the key value pair from dictionary at the hash value
-            if not self.collection[hash_value]:#use the not operator to check if the dictionary at the hash value is empty after removing the key-value
-               del self.collection[hash_value]#remove hash value from the collection
-    def lookup(self,key):
+        if hash_value in self.collection and key in self.collection[hash_value]:
+            del self.collection[hash_value][key]
+            if not self.collection[hash_value]:
+               del self.collection[hash_value]
         hash_value=self.hash(key)
         if hash_value in self.collection and key in self.collection[hash_value]:
-           return self.collection[hash_value][key]#it return the value of th key in that hash value
+           return self.collection[hash_value][key]
         return None 
 
 
